c9d636ec9171c568fbcf0d2d4c8885c7d934d359
Anderson Carneiro Sousa <acarneirosousa@gmail.com>
2023-07-14 14:55:01 -0300

    creating projet

---
+++ 2-Ignite-Time/src/App.tsx
@@ -0,0 +1,18 @@
+import { ThemeProvider } from 'styled-components'
+
+import { Button } from './components/Button/Button'
+import { defaultTheme } from './components/styles/themes/default'
+import { GlobalStyle } from './components/styles/global'
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="danger" />
+      <Button variant="success" />
+      <Button />
+      <GlobalStyle />
+    </ThemeProvider>
+  )
+}
